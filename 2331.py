a76535e6234668182f04a57df9ecd4b90d073ae6
pushpull7381 <pushpull7381@gmail.com>
2023-01-26 14:30:19 +0900

    delete

--- 2331.py
+++
@@ -1,6 +0,0 @@
-a, p = map(int, input().split())
-arr = []
-while a not in arr:
-    arr.append(a)
-    a = sum([int(i)**p for i in str(a)])
-print(arr.index(a))
--- 2338.cpp
+++
@@ -1,9 +0,0 @@
-#include "bits/stdc++.h"
-using namespace std;
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    long long A,B; cin >> A >> B;
-    cout << A+B << '\n'<< A-B << '\n'<< A*B;
-}
